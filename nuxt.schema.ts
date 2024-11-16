17dbab95910632ebd89dd8ff61bfa5a847e09fc4
Ilya M. <wuvuxd@proton.me>
2024-11-17 01:43:33 +0700

    chore(app): edit app schema

--- nuxt.schema.ts
+++ nuxt.schema.ts
@@ -2,19 +2,46 @@ import { field, group } from "@nuxthq/studio/theme";
 
 export default defineNuxtSchema({
   appConfig: {
-    parent: group({
-      title: "UI",
-      description: "UI configuration",
-      icon: "i-ph-palette-fill",
+    content: group({
+      title: "Content",
+      description: "Content configuration",
+      icon: "fluent:content-view-16-filled",
       fields: {
-        primary: field({
-          type: "string",
-          title: "Primary",
-          description: "Primary color of your UI.",
-          icon: "i-ph-palette",
-          default: "sky",
-          required: ["sky", "mint", "rose", "amber"],
+        workBackground: field({
+          type: "object",
+          title: "Work Background",
+          description: "All your jobs per year",
+          fields: {
+            startYear: field({
+              type: "number",
+              title: "Start Year",
+            }),
+            endYear: field({
+              type: "number",
+              title: "End Year",
+            }),
+            title: field({
+              type: "string",
+              title: "Job Title",
+            }),
+            location: field({
+              type: "string",
+              title: "Location",
+            }),
+            desctiption: field({
+              type: "default",
+              title: "Desctiption",
+            }),
+          },
         }),
+        // primary: field({
+        //   type: "string",
+        //   title: "Primary",
+        //   description: "Primary color of your UI.",
+        //   icon: "fluent:content-view-16-filled",
+        //   default: "sky",
+        //   required: ["sky", "mint", "rose", "amber"],
+        // }),
       },
     }),
   },
