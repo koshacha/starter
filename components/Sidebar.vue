d7fc91b336b88d51831e17dba6cd90bd471fe47d
Ilya M. <wuvuxd@proton.me>
2024-12-28 22:41:38 +0700

    chore: remove boilerplate-code

---
+++ components/Sidebar.vue
@@ -0,0 +1,5 @@
+<template>
+  <aside
+    class="z-50 hidden bg-white dark:bg-neutral-900 lg:flex md:w-72 md:flex-col md:fixed md:inset-y-0"
+  ></aside>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="min-h-[640px] flex flex-col">
+    <Sidebar />
+    <Content><slot /></Content>
+  </div>
+</template>
